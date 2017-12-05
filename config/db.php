15cf349ddcf7500b1ddf6a6d68e36844bf00ef89
Stanislav <idc.zaturinskyStanisalv@gmail.com>
2017-12-05 02:36:54 +0200

    remove view login

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ajaxchat',
+    'dsn' => 'mysql:host=localhost;dbname=ajaxchat2ajaxchat2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
